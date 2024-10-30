4b5e2ed1b698be7760e8356fbb26a3d7d9ce625e
Kawfang-ThTn <thanthong.somwijit@gmail.com>
2024-10-30 13:39:23 +0700

    test 01

--- to-do-app/src/App.jsx
+++ to-do-app/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const addTodo = () => {
     if( input.trim() ){
 
-      setTodo( [...prevTodo , {id : Date.now() , text : input , completed : false} ] )
+      setTodo( [...todo , {id : Date.now() , text : input , completed : false} ] )
       
       setInput("")
     }
@@ -26,18 +26,23 @@ function App() {
             <button className="bg-blue-400 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600" onClick={addTodo} >Add</button>
           </div>
 
-        </div>
+        
 
         <ul className="space-y-2">
           {
-            todo.map((element) => {
-              <li key={element.id} className="flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200" >
-                <input type="checkbox" checked={element.completed} onChange={ () => setTodo(todo.map((telement) => {telement.id === todo.id ? {...prevTelement , completed : !telement.completed} : telement}))} className="mr-2 h-5 w-5 text-blue-400" />
+            todo.map((todo) => (
+              <li key={todo.id} className="flex items-center p-3 rounded-lg bg-slate-100 border border-gray-200" >
                 
+                <input type="checkbox" checked={todo.completed} onChange={ () => setTodo(todo.map((t) => (t.id === todo.id ? {...t , completed : !t.completed} : t)))} className="mr-2 h-5 w-5 text-blue-400" />
+                <span className={`flex-grow ${todo.completed ? "line-through text-gray-400" : "text-gray-800"}`} >{todo.text}</span>
+                <button onClick={() => setTodo(todo.filter((t) => t.id !== todo.id))} className="ml-2 border-none p-2 rounded-lg bg-red-400 text-white hover:bg-red-600" >Delete</button>
+
               </li>
-            })
+            ))
           }
         </ul>
+
+        </div>
 
       </div>
     </>
